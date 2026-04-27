7ea79b1d194723fa3b8bdcd3d8bb62c25e4990da
Aditi Muskan <muskan.aditi1@gmail.com>
2026-04-28 02:18:16 +0530

    UC3

---
+++ UC3.java
@@ -0,0 +1,17 @@
+UC3: Accept User Slot Input (1–9)
+Goal: Allow the user to enter a slot number between 1 and 9.
+
+Actor: Human Player
+
+Flow:
+User enters slot number → program reads input → control returns to game logic.
+
+Key Concepts used in UC3:
+
+User Input Handling
+Method Creation
+Separation of Concerns
+Key Requirements:
+
+Read integer input
+Return the slot value
